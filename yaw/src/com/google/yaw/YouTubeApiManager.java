40034a02e07bf20163fcb86f2e72bb4c2a356a84
api.jeffy <api.jeffy@9dcab996-c835-11de-8672-e51b9359e43c>
2009-08-24 14:42:40 +0000

    Updated the logic for retrieving valid YouTube categories to actually fetch the XML list from a remote server and parse it properly, instead of hardcoding them.

--- yaw/src/com/google/yaw/YouTubeApiManager.java
+++ yaw/src/com/google/yaw/YouTubeApiManager.java
@@ -1,17 +1,37 @@
 package com.google.yaw;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.cache.Cache;
+import javax.cache.CacheException;
+import javax.cache.CacheManager;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import com.google.appengine.api.memcache.stdimpl.GCacheFactory;
 import com.google.gdata.client.youtube.YouTubeService;
 import com.google.gdata.data.youtube.FormUploadToken;
 import com.google.gdata.data.youtube.UserProfileEntry;
 import com.google.gdata.data.youtube.VideoEntry;
 import com.google.gdata.util.ServiceException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * Class to handle interfacing with the Google Data Java Client Library's
@@ -20,6 +40,7 @@ import com.google.gdata.util.ServiceException;
 public class YouTubeApiManager {
 
 	private YouTubeService service = null;
+	private static final String categoriesCacheKey = "categories";
 	private static final String userEntry = "http://gdata.youtube.com/feeds/api/users/default";
 	private static final String uploadToken = "http://gdata.youtube.com/action/GetUploadToken";
 	private static final Logger log = Logger.getLogger(YouTubeApiManager.class.getName());
@@ -98,15 +119,69 @@ public class YouTubeApiManager {
 		return token;
 	}
 
-	public static List<String> getCategoryCodes() {
-		// TODO: parse data from
-		// http://gdata.youtube.com/schemas/2007/categories.cat
-		// instead of returning a static array.
+	@SuppressWarnings("unchecked")
+  public static List<String> getCategoryCodes() {
+	  List<String> categories;
+	  Cache cache = null;
+	  
+	  try {
+	    Map cachedProperties = new HashMap();
+	    cachedProperties.put(GCacheFactory.EXPIRATION_DELTA, 60 * 60 * 24);
+	    cache = CacheManager.getInstance().getCacheFactory().createCache(cachedProperties);
+	    List<String> cachedCategories = (List<String>)cache.get(categoriesCacheKey);
+	    
+	    if (cachedCategories != null) {
+	      return cachedCategories;
+	    }
+	  } catch (CacheException e) {
+	    log.log(Level.WARNING, "", e);
+	  }
+	  
+	  categories = new ArrayList<String>();
 
-		List<String> categories = new ArrayList<String>();
-		categories.add("News");
-		categories.add("Sports");
-		categories.add("Music");
+	  try {
+	    URL url = new URL("http://gdata.youtube.com/schemas/2007/categories.cat");
+      DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+      Document xmlDocument = docBuilder.parse(url.openStream());
+      
+      NodeList nodes = xmlDocument.getElementsByTagName("atom:category");
+      for (int i = 0; i < nodes.getLength(); i++) {
+        Node node = nodes.item(i);
+        
+        boolean isAssignable = false;
+        NodeList childNodes = node.getChildNodes();
+        for (int j = 0; j < childNodes.getLength(); j++) {
+          Node childNode = childNodes.item(j);
+          if (childNode.getNodeName().equals("yt:assignable")) {
+            isAssignable = true;
+          }
+        }
+        
+        if (isAssignable) {
+          NamedNodeMap attributes = node.getAttributes();
+          Node termNode = attributes.getNamedItem("term");
+
+          if (termNode != null) {
+            categories.add(termNode.getTextContent());
+          }
+        }
+      }
+      
+      Collections.sort(categories);
+      
+      if (cache != null) {
+        cache.put(categoriesCacheKey, categories);
+      }
+	  } catch (MalformedURLException e) {
+	    log.log(Level.WARNING, "", e);
+	  } catch (ParserConfigurationException e) {
+	    log.log(Level.WARNING, "", e);
+    } catch (IOException e) {
+      log.log(Level.WARNING, "", e);
+    } catch (SAXException e) {
+      log.log(Level.WARNING, "", e);
+    }
 
 		return categories;
 	}
--- yaw/src/com/google/yaw/admin/GetAllAssignments.java
+++ yaw/src/com/google/yaw/admin/GetAllAssignments.java
@@ -20,6 +20,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet to handle requests that fetch Assignments from the datastore.
+ * 
+ * <p>Handles both requests to fetch all assignment, and requests for a subset of assignments
+ * (i.e. filtered with search parameters or paged requests).
+ */
 public class GetAllAssignments extends HttpServlet {
   private static final Logger log = Logger.getLogger(GetAllAssignments.class.getName());
 
@@ -30,24 +36,48 @@ public class GetAllAssignments extends HttpServlet {
     PersistenceManager pm = pmf.getPersistenceManager();
 
     try {
-      //TODO: Check these for null|empty strings.
       String search = req.getParameter("_search");
       String sortColumn = req.getParameter("sidx");
       String sortOrder = req.getParameter("sord");
       String pageNumberParam = req.getParameter("page");
       String pageSizeParam = req.getParameter("rows");
 
+      if (Util.isNullOrEmpty(sortColumn)) {
+        throw new IllegalArgumentException("'sidx' parameter is null or empty.");
+      }
+      if (Util.isNullOrEmpty(sortOrder)) {
+        throw new IllegalArgumentException("'sord' parameter is null or empty.");
+      }
+      if (Util.isNullOrEmpty(pageNumberParam)) {
+        throw new IllegalArgumentException("'page' parameter is null or empty.");
+      }
+      if (Util.isNullOrEmpty(pageSizeParam)) {
+        throw new IllegalArgumentException("'rows' parameter is null or empty.");
+      }
+      
       int pageNumber = Integer.parseInt(pageNumberParam);
       int pageSize = Integer.parseInt(pageSizeParam);
 
+      // Query the datastore for all Assignments, with the desired sort order.
       Query query = pm.newQuery(Assignment.class);
       query.setOrdering(String.format("%s %s", sortColumn, sortOrder));
       List<Assignment> assignments = (List<Assignment>)query.execute();
 
       if (search != null && search.equals("true")) {
+        // The request is for a filtered subset of the full results.
         String searchColumn = req.getParameter("searchField");
         String searchString = req.getParameter("searchString");
         String searchType = req.getParameter("searchOper");
+
+        if (Util.isNullOrEmpty(searchColumn)) {
+          throw new IllegalArgumentException("'searchField' parameter is null or empty.");
+        }
+        if (Util.isNullOrEmpty(searchString)) {
+          throw new IllegalArgumentException("'searchString' parameter is null or empty.");
+        }
+        if (Util.isNullOrEmpty(searchType)) {
+          throw new IllegalArgumentException("'searchOper' parameter is null or empty.");
+        }
 
         // Mimic case-insensitive search by converting everything to lower case.
         searchString = searchString.toLowerCase();
@@ -84,16 +114,32 @@ public class GetAllAssignments extends HttpServlet {
           }
         }
 
+        // Replace the reference to the full list with the temporary list.
         assignments = filteredAssignments;
       }
 
       int totalResults = assignments.size();
+      
+      // If request is for 3rd page of results, and each page has 5 results, the first result to
+      // return would be index 10.
       int startIndex = (pageNumber - 1) * pageSize;
+
+      // 1 greater than the index of the last element to include, because the second parameter to
+      // List.subList() is interpretted as being exclusive. If request is for 3rd page of results,
+      // and each page has 5 results, the index value to use would be 15.
       int stopIndex = pageNumber * pageSize;
+      
+      // Total number of pages is equal to the integer greater than the total divided by page size.
+      // If there are 34 total results and a page size of 5, total pages is 7.
       int totalPages = (int)Math.ceil((double)totalResults / (double)pageSize);
+      
       if (stopIndex > totalResults) {
+        // If this is the last page of results than the calculated index might be bigger than the
+        // last valid index in the list.
         stopIndex = totalResults;
       }
+      
+      // 0-based. startIndex is inclusive, stopIndex is exclusive.
       assignments = assignments.subList(startIndex, stopIndex);
 
       // The JSON response matches the format expected by jqGrid, as documented at
@@ -113,6 +159,8 @@ public class GetAllAssignments extends HttpServlet {
         String submissionCount = Integer.toString(assignment.getSubmissionCount());
 
         JSONObject row = new JSONObject();
+        // Each row needs a unique id in addition to the user-defined "id" column. Instead of using
+        // some incremental number, use datastore id, which should be unique.
         row.put("id", id);
 
         List<String> data = new ArrayList<String>();
