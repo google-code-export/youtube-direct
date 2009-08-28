c494a33ede4a0fd4bd951055c7f97298f47f39f0
api.jeffy <api.jeffy@9dcab996-c835-11de-8672-e51b9359e43c>
2009-08-28 19:52:14 +0000

    When updating a VideoSubmission to mark it as approved, we now prepend some branding text containing the original article's URL in the video's description.
    Also updated the logging.properties file to mask out some WARNING level messages caused by the GData client library.

--- yaw/src/com/google/yaw/admin/UpdateSubmission.java
+++ yaw/src/com/google/yaw/admin/UpdateSubmission.java
@@ -2,6 +2,7 @@ package com.google.yaw.admin;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.jdo.PersistenceManager;
@@ -10,7 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gdata.data.youtube.VideoEntry;
+import com.google.gdata.util.ServiceException;
 import com.google.yaw.Util;
+import com.google.yaw.YouTubeApiManager;
 import com.google.yaw.model.VideoSubmission;
 import com.google.yaw.model.VideoSubmission.ModerationStatus;
 
@@ -18,6 +22,7 @@ public class UpdateSubmission extends HttpServlet {
 
   private static final Logger log = Logger.getLogger(UpdateSubmission.class.getName());
 
+  @SuppressWarnings("cast")
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
@@ -51,6 +56,16 @@ public class UpdateSubmission extends HttpServlet {
       entry.setVideoDescription(jsonObj.getVideoDescription());
       entry.setVideoTags(jsonObj.getVideoTags());
       entry.setUpdated(new Date());
+      
+      if (currentStatus != newStatus && newStatus == ModerationStatus.APPROVED) {
+        //TODO: Move this to a properties file setting.
+        String prependText = String.format("Uploaded in response to %s", entry.getArticleUrl());
+        
+        if (!entry.getVideoDescription().startsWith(prependText)) {
+          // We only want to update the video if the text isn't already there.
+          UpdateVideoDescription(entry, prependText);
+        }
+      }
 
       pm.makePersistent(entry);
       // FullTextIndexer.addIndex(entry, entry.getClass());
@@ -65,5 +80,53 @@ public class UpdateSubmission extends HttpServlet {
     } finally {
       pm.close();
     }
+  }
+  
+  /**
+   * Updates the description of a video, both in the datastore and on YouTube, to prepend the
+   * "branding" text.
+   * This should be called when a video submission is marked for approval.
+   * Note that the VideoSubmission is updated in-memory, but a call to
+   * PersistenceManager.makePersistent(VideoSubmission) must be made by the calling code to save
+   * the changes to the datastore.
+   * @param videoSubmission The datastore object that is to be changed. Note that this parameter
+   * will be modified by this method and must be persisted by the calling code.
+   * @param prependText The text that should be prepending to the video's description.
+   * @return A YouTube API VideoEntry object with the updated description, or null if the video
+   * could not be updated.
+   */
+  private VideoEntry UpdateVideoDescription(VideoSubmission videoSubmission, String prependText) {
+    String videoId = videoSubmission.getVideoId();
+    log.info(String.format("Updating description of submission id '%s' (YouTube video id '%s').",
+            videoSubmission.getId(), videoId));
+
+    YouTubeApiManager apiManager = new YouTubeApiManager();
+    apiManager.setToken(videoSubmission.getAuthSubToken());
+    
+    VideoEntry videoEntry = apiManager.getVideoEntry(videoId);
+    if (videoEntry == null) {
+      log.warning(String.format("Couldn't get video with id '%s' in the uploads feed of user " +
+      		"'%s'. Perhaps the AuthSub token has been revoked?", videoId,
+      		videoSubmission.getUploader()));
+    } else {
+      String currentText = videoSubmission.getVideoDescription();
+      String newText = String.format("%s\n\n%s", prependText, currentText);
+      
+      // Update the datastore entry.
+      videoSubmission.setVideoDescription(newText);
+      
+      videoEntry.getMediaGroup().getDescription().setPlainTextContent(newText);
+      try {
+        // And update the YouTube.com video as well.
+        videoEntry.update();
+        return videoEntry;
+      } catch (IOException e) {
+        log.log(Level.WARNING, String.format("Error while updating video id '%s':", videoId), e);
+      } catch (ServiceException e) {
+        log.log(Level.WARNING, String.format("Error while updating video id '%s':", videoId), e);
+      }
+    }
+    
+    return null;
   }
 }
