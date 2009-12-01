811e435eda0f84c1e98e13c6e483cf2b5a06756d
austin.chau <austin.chau@9dcab996-c835-11de-8672-e51b9359e43c>
2009-12-01 08:19:19 +0000

    - correct way to do mock

--- branches/2.0/test/com/google/ytd/jsonrpc/BasicCommandTest.java
+++ branches/2.0/test/com/google/ytd/jsonrpc/BasicCommandTest.java
@@ -15,10 +15,7 @@ import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.google.ytd.command.Command;
+import com.google.ytd.command.GetSubmissionsCommand;
 import com.google.ytd.dao.SubmissionManager;
 import com.google.ytd.model.VideoSubmission;
 
@@ -29,36 +26,27 @@ public class BasicCommandTest{
 
   @Test
   public void testGetSubmissions() throws JSONException {
-    AbstractModule testModule = new AbstractModule() {
-      @Override
-      protected void configure() {
-        final VideoSubmission videoSubmission = new VideoSubmission(1l);
-        videoSubmission.setArticleUrl("blah");
-        final List<VideoSubmission> submissions = new ArrayList<VideoSubmission>();
-        submissions.add(videoSubmission);
+    final VideoSubmission videoSubmission = new VideoSubmission(1l);
+    videoSubmission.setArticleUrl("blah");
+    final List<VideoSubmission> submissions = new ArrayList<VideoSubmission>();
+    submissions.add(videoSubmission);
 
-        JUnit4Mockery mockery = new JUnit4Mockery();
-        final SubmissionManager manager = mockery.mock(SubmissionManager.class);
-        mockery.checking(new Expectations() {{
-          oneOf(manager).getSubmissions(with("created"), with("desc"), with("all"));
-          will(returnValue(submissions));
-        }});
+    JUnit4Mockery mockery = new JUnit4Mockery();
+    final SubmissionManager manager = mockery.mock(SubmissionManager.class);
+    mockery.checking(new Expectations() {{
+      oneOf(manager).getSubmissions(with("created"), with("desc"), with("all"));
+      will(returnValue(submissions));
+    }});
 
-        this.bind(SubmissionManager.class).toInstance(manager);
-      }
-    };
-
-    Injector injector = Guice.createInjector(testModule);
-    CommandDirectory commandDirectory = new CommandDirectory(injector);
-
-    String method = "ytd.getSubmissions";
+    GetSubmissionsCommand command = new GetSubmissionsCommand(manager);
     Map<String,String> params = new HashMap<String,String>();
     params.put("sortBy", "created");
     params.put("sortOrder", "desc");
     params.put("filterType", "all");
     params.put("pageIndex", "1");
     params.put("pageSize", "10");
-    Command command = commandDirectory.getCommand(method, params);
+    command.setParams(params);
+
     JSONObject response = command.execute();
     assertNotNull("JSONObject is null", response);
     assertTrue("result length is zero", response.getInt("totalPages") > 0);
