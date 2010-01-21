8899d8382b4e0ac4a2b2f8e3642862cc4f2d4068
austin.chau <austin.chau@9dcab996-c835-11de-8672-e51b9359e43c>
2010-01-21 00:41:41 +0000

    - misc refactor

--- branches/2.0/src/com/google/ytd/command/UpdateAdminConfigCommand.java
+++ branches/2.0/src/com/google/ytd/command/UpdateAdminConfigCommand.java
@@ -15,100 +15,101 @@ import com.google.ytd.model.AdminConfig.SubmissionModeType;
 import com.google.ytd.util.Util;
 
 public class UpdateAdminConfigCommand extends Command {
-  private AdminConfigDao adminConfigDao = null;
+	private AdminConfigDao adminConfigDao = null;
 
-  private static final Logger LOG = Logger.getLogger(UpdateAdminConfigCommand.class.getName());
+	private static final Logger LOG = Logger.getLogger(UpdateAdminConfigCommand.class.getName());
 
-  @Inject
-  private Util util;
+	@Inject
+	private Util util;
 
-  @Inject
-  public UpdateAdminConfigCommand(AdminConfigDao adminConfigDao) {
-    this.adminConfigDao = adminConfigDao;
-  }
-  @Override
-  public JSONObject execute() throws JSONException {
-    LOG.info(this.toString());
-    JSONObject json = new JSONObject();
-    String clientId = getParam("clientId");
-    String developerKey = getParam("developerKey");
-    String defaultTag = getParam("defaultTag");
-    String linkBackText = getParam("inkBackText");
-    String moderationMode = getParam("moderationMode");
-    String brandingMode = getParam("brandingMode");
-    String submissionMode = getParam("submissionMode");
-    String newSubmissionAddress = getParam("newSubmissionAddress");
-    String loginInstruction = getParam("loginInstruction");
-    String postSubmitMessage = getParam("postSubmitMessage");
-    String moderationEmail = getParam("moderationEmail");
-    String fromAddress = getParam("fromAddress");
-    String approvalEmailText = getParam("approvalEmailText");
-    String rejectionEmailText = getParam("rejectionEmailText");
+	@Inject
+	public UpdateAdminConfigCommand(AdminConfigDao adminConfigDao) {
+		this.adminConfigDao = adminConfigDao;
+	}
 
-    AdminConfig adminConfig = adminConfigDao.getAdminConfig();
+	@Override
+	public JSONObject execute() throws JSONException {
+		LOG.info(this.toString());
+		JSONObject json = new JSONObject();
+		String clientId = getParam("clientId");
+		String developerKey = getParam("developerKey");
+		String defaultTag = getParam("defaultTag");
+		String linkBackText = getParam("inkBackText");
+		String moderationMode = getParam("moderationMode");
+		String brandingMode = getParam("brandingMode");
+		String submissionMode = getParam("submissionMode");
+		String newSubmissionAddress = getParam("newSubmissionAddress");
+		String loginInstruction = getParam("loginInstruction");
+		String postSubmitMessage = getParam("postSubmitMessage");
+		String moderationEmail = getParam("moderationEmail");
+		String fromAddress = getParam("fromAddress");
+		String approvalEmailText = getParam("approvalEmailText");
+		String rejectionEmailText = getParam("rejectionEmailText");
 
-    if (!util.isNullOrEmpty(clientId)) {
-      adminConfig.setClientId(clientId);
-    }
+		AdminConfig adminConfig = adminConfigDao.getAdminConfig();
 
-    if (!util.isNullOrEmpty(developerKey)) {
-      adminConfig.setDeveloperKey(developerKey);
-    }
+		if (!util.isNullOrEmpty(clientId)) {
+			adminConfig.setClientId(clientId);
+		}
 
-    if (!util.isNullOrEmpty(defaultTag)) {
-      adminConfig.setDefaultTag(defaultTag);
-    }
+		if (!util.isNullOrEmpty(developerKey)) {
+			adminConfig.setDeveloperKey(developerKey);
+		}
 
-    if (!util.isNullOrEmpty(linkBackText)) {
-      adminConfig.setLinkBackText(linkBackText);
-    }
+		if (!util.isNullOrEmpty(defaultTag)) {
+			adminConfig.setDefaultTag(defaultTag);
+		}
 
-    if (!util.isNullOrEmpty(moderationMode)) {
-      adminConfig.setModerationMode(ModerationModeType.valueOf(
-          moderationMode.toUpperCase()).ordinal());
-    }
+		if (!util.isNullOrEmpty(linkBackText)) {
+			adminConfig.setLinkBackText(linkBackText);
+		}
 
-    if (!util.isNullOrEmpty(brandingMode)) {
-      adminConfig.setBrandingMode(BrandingModeType.valueOf(brandingMode.toUpperCase()).ordinal());
-    }
+		if (!util.isNullOrEmpty(moderationMode)) {
+			adminConfig.setModerationMode(ModerationModeType.valueOf(moderationMode.toUpperCase())
+					.ordinal());
+		}
 
-    if (!util.isNullOrEmpty(submissionMode)) {
-      adminConfig.setSubmissionMode(SubmissionModeType.valueOf(
-          submissionMode.toUpperCase()).ordinal());
-    }
+		if (!util.isNullOrEmpty(brandingMode)) {
+			adminConfig.setBrandingMode(BrandingModeType.valueOf(brandingMode.toUpperCase()).ordinal());
+		}
 
-    if (!util.isNullOrEmpty(newSubmissionAddress)) {
-      adminConfig.setNewSubmissionAddress(newSubmissionAddress);
-    }
+		if (!util.isNullOrEmpty(submissionMode)) {
+			adminConfig.setSubmissionMode(SubmissionModeType.valueOf(submissionMode.toUpperCase())
+					.ordinal());
+		}
 
-    if (!util.isNullOrEmpty(loginInstruction)) {
-      adminConfig.setLoginInstruction(loginInstruction);
-    }
+		if (!util.isNullOrEmpty(newSubmissionAddress)) {
+			adminConfig.setNewSubmissionAddress(newSubmissionAddress);
+		}
 
-    if (!util.isNullOrEmpty(postSubmitMessage)) {
-      adminConfig.setPostSubmitMessage(postSubmitMessage);
-    }
+		if (!util.isNullOrEmpty(loginInstruction)) {
+			adminConfig.setLoginInstruction(loginInstruction);
+		}
 
-    if (!util.isNullOrEmpty(moderationEmail)) {
-      boolean isModerationEmailOn = moderationEmail.toLowerCase().equals("true");
-      adminConfig.setModerationEmail(isModerationEmailOn);
-    }
+		if (!util.isNullOrEmpty(postSubmitMessage)) {
+			adminConfig.setPostSubmitMessage(postSubmitMessage);
+		}
 
-    if (!util.isNullOrEmpty(fromAddress)) {
-      adminConfig.setFromAddress(fromAddress);
-    }
+		if (!util.isNullOrEmpty(moderationEmail)) {
+			boolean isModerationEmailOn = moderationEmail.toLowerCase().equals("true");
+			adminConfig.setModerationEmail(isModerationEmailOn);
+		}
 
-    if (!util.isNullOrEmpty(approvalEmailText)) {
-      adminConfig.setApprovalEmailText(approvalEmailText);
-    }
+		if (!util.isNullOrEmpty(fromAddress)) {
+			adminConfig.setFromAddress(fromAddress);
+		}
 
-    if (!util.isNullOrEmpty(rejectionEmailText)) {
-      adminConfig.setRejectionEmailText(rejectionEmailText);
-    }
+		if (!util.isNullOrEmpty(approvalEmailText)) {
+			adminConfig.setApprovalEmailText(approvalEmailText);
+		}
 
-    adminConfig.setUpdated(new Date());
-    adminConfig = adminConfigDao.save(adminConfig);
+		if (!util.isNullOrEmpty(rejectionEmailText)) {
+			adminConfig.setRejectionEmailText(rejectionEmailText);
+		}
 
-    return json;
-  }
+		adminConfig.setUpdated(new Date());
+		adminConfig = adminConfigDao.save(adminConfig);
+
+		return json;
+	}
 }
