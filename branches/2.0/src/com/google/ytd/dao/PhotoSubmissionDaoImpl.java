eb332f3b50ba9b36d3bc049e9247a30090f3c068
austin.chau <austin.chau@9dcab996-c835-11de-8672-e51b9359e43c>
2010-01-22 05:16:48 +0000

    - Photo models and dao classes

---
+++ branches/2.0/src/com/google/ytd/dao/PhotoSubmissionDaoImpl.java
@@ -0,0 +1,21 @@
+package com.google.ytd.dao;
+
+import java.util.List;
+
+import com.google.ytd.model.PhotoSubmission;
+
+public class PhotoSubmissionDaoImpl implements PhotoSubmissionDao {
+
+  @Override
+  public List<PhotoSubmission> getAllPhotoSubmissionsById(String assignmentId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public PhotoSubmission getPhotoSubmission(String photoId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
