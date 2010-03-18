581c35fdafd6abd89a05e2d9668c9970848eaf89
api.jeffy <api.jeffy@9dcab996-c835-11de-8672-e51b9359e43c>
2010-03-18 21:14:00 +0000

    Added some admin-only protection for the 2.0 branch. All JSON-RPC calls are assumed to be admin-only unless the @NonAdmin annotation is applied to the underlying Command class.

---
+++ src/com/google/ytd/command/NonAdmin.java
@@ -0,0 +1,9 @@
+package com.google.ytd.command;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NonAdmin {
+
+}
