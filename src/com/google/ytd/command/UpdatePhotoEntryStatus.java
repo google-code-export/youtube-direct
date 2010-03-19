1dcde64e004786cf37a49b1d9df55c88a5bc384a
austin.chau <austin.chau@9dcab996-c835-11de-8672-e51b9359e43c>
2010-03-19 17:32:54 +0000

    - add UpdatePhotoEntryStatus command

---
+++ src/com/google/ytd/command/UpdatePhotoEntryStatus.java
@@ -0,0 +1,50 @@
+package com.google.ytd.command;
+
+import java.util.logging.Logger;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.inject.Inject;
+import com.google.ytd.dao.PhotoSubmissionDao;
+import com.google.ytd.model.PhotoEntry;
+import com.google.ytd.model.PhotoEntry.ModerationStatus;
+import com.google.ytd.util.Util;
+
+public class UpdatePhotoEntryStatus extends Command {
+  private static final Logger LOG = Logger.getLogger(UpdatePhotoEntryStatus.class.getName());
+
+  private PhotoSubmissionDao submissionDao = null;
+
+  @Inject
+  private Util util;
+
+  @Inject
+  public UpdatePhotoEntryStatus(PhotoSubmissionDao submissionDao) {
+    this.submissionDao = submissionDao;
+  }
+
+  @Override
+  public JSONObject execute() throws JSONException {
+    LOG.info(this.toString());
+    JSONObject json = new JSONObject();
+    String id = getParam("id");
+    String status = getParam("status");
+
+    if (util.isNullOrEmpty(id)) {
+      throw new IllegalArgumentException("Missing required param: id");
+    }
+    if (util.isNullOrEmpty(status)) {
+      throw new IllegalArgumentException("Missing required param: status");
+    }
+     
+    PhotoEntry entry = submissionDao.getPhotoEntry(id);
+    
+    ModerationStatus newStatus = ModerationStatus.valueOf(status.toUpperCase());
+    ModerationStatus currentStatus = entry.getStatus();
+    
+    entry.setStatus(newStatus);
+    
+    return json;
+  }
+}
